8fa70a13184cea18fb1d40cfdb1e24bc87f7209a
𝖄𝒀𝒂𝐫𝒐𝒔𝒍𝐚𝒗𝖄 <d9127506@gmail.com>
2025-06-18 16:55:19 +0300

    new update

--- Program.cs
+++ Program.cs
@@ -6,67 +6,52 @@ namespace RayTracer
 {
     public class Program
     {
-        private static double Colors = 255;
+        private const double Colors = 256;
 
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            int photWidth = 640;
-            int photHeight = 360;
-            string output = "output.ppm";
+            int nx = 200;
+            int ny = 100;
+            var camera = new Camera();
+            var sphere = new Sphere(new Vec3(0, 0, -1), 0.5);
+            var sb = new StringBuilder();
 
-            using (StreamWriter writer = new StreamWriter(output))
+            sb.AppendLine("P3");
+            sb.AppendLine($"{nx} {ny}");
+            sb.AppendLine("255");
+
+            for (int j = ny - 1; j >= 0; j--)
             {
-                writer.WriteLine("P3");
-                writer.WriteLine(photWidth + " " + photHeight);
-                writer.WriteLine("255");
-
-                var world = new Hitable();
-                world.Add(new Sphere(new Vec3(0, 0, -1), 0.5));
-                world.Add(new Sphere(new Vec3(0, -100, -1), 100));
-
-                var camera = new Camera();
-
-                for (int y = photHeight - 1; y >= 0; y--)
+                for (int i = 0; i < nx; i++)
                 {
-                    for (int x = 0; x < photWidth; x++)
-                    {
-                        double u = (double)x / (photWidth - 1);
-                        double v = (double)y / (photHeight - 1);
-                        Ray ray = camera.GetRay(u, v);
-                        Vec3 color = RayColor(ray, world);
-                        WriteColor(writer, color);
-                    }
+                    double u = (double)i / (nx - 1);
+                    double v = (double)j / (ny - 1);
+
+                    var ray = camera.GetRay(u, v);
+                    var color = RayColor(ray, sphere);
+
+                    int red = (int)(color.X * Colors);
+                    int green = (int)(color.Y * Colors);
+                    int blue = (int)(color.Z * Colors);
+
+                    sb.AppendLine($"{red} {green} {blue}");
                 }
             }
 
-            Console.WriteLine("Рендеринг завершено!!! Переглянути зображення можна в файлах...");
+            File.WriteAllText("output1.ppm", sb.ToString());
         }
 
-        private static Vec3 RayColor(Ray r, Hitable world)
+        static Vec3 RayColor(Ray ray, Sphere sphere)
         {
-            HitRecord rec;
-            if (world.Hit(r, 0.001, double.MaxValue, out rec))
+            if (sphere.Hit(ray, 0.001, double.MaxValue, out HitRecord rec))
             {
-                Vec3 N = rec.Normal;
-                return new Vec3(N.X + 1, N.Y + 1, N.Z + 1).Multiply(0.5);
+                return new Vec3(0.5 * (rec.Normal.X + 1.0), 0.5 * (rec.Normal.Y + 1.0),0.5 * (rec.Normal.Z + 1.0) );
             }
 
-            Vec3 rayDirection = r.Direction.Unit();
-            double t = 0.5 * (rayDirection.Y + 1);
-            return new Vec3(1, 1, 1).Multiply(1 - t).Add(new Vec3(0, 0, 1).Multiply(t));
+            var unitDir = ray.Direction.Unit();
+            double t = 0.5 * (unitDir.Y + 1.0);
+            return new Vec3( (1.0 - t) * 1.0 + t * 0.5,(1.0 - t) * 1.0 + t * 0.7,(1.0 - t) * 1.0);
         }
-
-
-        private static void WriteColor(StreamWriter writer, Vec3 color)
-        {
-            int red = (int)(color.X * Colors);
-            int green = (int)(color.Y * Colors);
-            int blue = (int)(color.Z * Colors);
-
-            string rgbLine = red + " " + green + " " + blue;
-
-            writer.WriteLine(rgbLine);
-        }
-
     }
 }
+
